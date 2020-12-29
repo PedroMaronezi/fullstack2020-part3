547208040ed3d8061c03fed842d4e1b3c2e15a91
Pedro Maronezi <pedromaronezi@usp.br>
2020-12-29 18:51:09 +0100

    3.7: Phonebook backend step7 & 3.8*: Phonebook backend step8

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 const { request, response } = require('express')
 const express = require('express')
+const morgan = require('morgan')
 const app = express()
 
 app.use(express.json())
+morgan.token('data', (req, res) => req.method === 'POST' ? JSON.stringify(req.body) : ' ')
+app.use(morgan(':method :url :status :res[content-length] - :response-time ms :data'))
 
 const generateId = () => {
     return Math.round(Math.random() * 100000)
