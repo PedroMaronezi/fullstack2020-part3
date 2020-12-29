e669c92f0b24ef2120f908a515856b6564bcd7e2
Pedro Maronezi <pedromaronezi@usp.br>
2020-12-29 17:53:19 +0100

    3.5: Phonebook backend step5 & 3.6: Phonebook backend step6

--- index.js
+++ index.js
@@ -1,7 +1,12 @@
+const { request, response } = require('express')
 const express = require('express')
 const app = express()
 
 app.use(express.json())
+
+const generateId = () => {
+    return Math.round(Math.random() * 100000)
+}
 
 let persons = [
     {
@@ -58,6 +63,31 @@ app.delete('/api/persons/:id', (request, response) => {
     persons = persons.filter(person => person.id !== id)
 
     response.status(204).end()
+})
+
+// POST a new person to the phonebook
+app.post('/api/persons', (request, response) => {
+    const body = request.body
+
+    if(!body.name){
+        return response.status(400).json({error: 'name missing'})
+    } 
+    if(!body.number){
+        return response.status(400).json({error: 'number missing'})
+    }
+    if(persons.find(person => person.name === body.name)){
+        return response.status(400).json({error: 'name must be unique'})
+    }
+
+    const person = {
+        id: generateId(),
+        name: body.name,
+        number: body.number
+    }
+
+    persons = persons.concat(person)
+
+    response.json(person)
 })
 
 
