dc5c6b2cc29009a9b32dab9aac0c4e9312e63fa1
Pedro Maronezi <pedromaronezi@usp.br>
2020-12-30 18:38:14 +0100

    3.14: Phonebook database, step2

--- index.js
+++ index.js
@@ -83,19 +83,16 @@ app.post('/api/persons', (request, response) => {
     if(!body.number){
         return response.status(400).json({error: 'number missing'})
     }
-    if(persons.find(person => person.name === body.name)){
-        return response.status(400).json({error: 'name must be unique'})
-    }
 
-    const person = {
-        id: generateId(),
+    const person = new Person({
         name: body.name,
         number: body.number
-    }
+    })
 
-    persons = persons.concat(person)
-
-    response.json(person)
+    person.save().then(savedPerson => {
+        console.log(`Added ${savedPerson.name} number ${savedPerson.number} to the phonebook`)
+        response.json(savedPerson)
+    })
 })
 
 
