99ee63d3a6a61e0f992d94180799cd5822302d0d
Pedro Maronezi <pedromaronezi@usp.br>
2020-12-31 16:52:52 +0100

    3.19: Phonebook database, step7 & 3.20*: Phonebook database, step8

--- index.js
+++ index.js
@@ -55,25 +55,20 @@ app.delete('/api/persons/:id', (request, response, next) => {
 })
 
 // POST a new person to the phonebook
-app.post('/api/persons', (request, response) => {
+app.post('/api/persons', (request, response, next) => {
     const body = request.body
-
-    if(!body.name){
-        return response.status(400).json({error: 'name missing'})
-    } 
-    if(!body.number){
-        return response.status(400).json({error: 'number missing'})
-    }
 
     const person = new Person({
         name: body.name,
         number: body.number
     })
 
-    person.save().then(savedPerson => {
-        console.log(`Added ${savedPerson.name} number ${savedPerson.number} to the phonebook`)
-        response.json(savedPerson)
-    })
+    person.save()
+        .then(savedPerson => {
+            console.log(`Added ${savedPerson.name} number ${savedPerson.number} to the phonebook`)
+            response.json(savedPerson)
+        })
+        .catch(error => next(error))
 })
 
 // PUT new information about an existing person
@@ -104,6 +99,8 @@ const errorHandler = (error, request, response, next) => {
 
     if(error.name === 'CastError'){
         return response.status(400).send({ error: 'malformatted id' })
+    } else if(error.name === 'ValidationError') {
+        return response.status(400).json({ error: error.message })
     }
 
     next(error)
